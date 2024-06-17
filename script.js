fa346eb20568ce8a73d38c5ff47671df11b33691
gym-corporation <artyom.akhmetov@gmail.com>
2024-06-18 00:59:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let score = 0;
 
 document.getElementById('coin').addEventListener('click', () => {
     score++;
-    document.getElementById('score').innerText = 'Score: ${score}';
+    document.getElementById('score').innerText = `Score: ${score}`;
 });
